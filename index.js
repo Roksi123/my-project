e10ff2e2554d8a4f7b25f3e9fa1093bc793aa1f8
Roksi123 <86662366+Roksi123@users.noreply.github.com>
2022-09-29 21:02:45 +0300

    added unit conversion

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  return `${day},${hours}:${minutes}`;
+  return `${day}, ${hours}:${minutes}`;
 }
 let newData = document.querySelector(`#currentData`);
 let currentTime = new Date();
@@ -33,18 +33,15 @@ function search(event) {
 let form = document.querySelector(`#dataType`);
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
+
+  celsiusTemperature = response.data.main.temp;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
 
   document.querySelector("#textDescription").innerHTML =
     response.data.weather[0].description;
-  document.querySelector("#highTem").innerHTML = Math.round(
-    response.data.main.temp_max
-  );
-  document.querySelector("#lowTem").innerHTML = Math.round(
-    response.data.main.temp_min
-  );
+
   document.querySelector("#windSp").innerHTML = Math.round(
     response.data.wind.speed
   );
@@ -76,6 +73,30 @@ function showPosition(position) {
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
+function displayFarenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiuslink.classList.remove("active");
+  farenheitlink.classList.add("active");
+  let farenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+
+  temperatureElement.innerHTML = Math.round(farenheitTemperature);
+}
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  celsiuslink.classList.add("active");
+  farenheitlink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusTemperature = null;
 let button = document.querySelector("#current-location-button");
 button.addEventListener("click", getCurrentPosition);
+
+let farenheitlink = document.querySelector("#fahrenheit-link");
+
+farenheitlink.addEventListener("click", displayFarenheitTemp);
+let celsiuslink = document.querySelector("#celsius-link");
+
+celsiuslink.addEventListener("click", displayCelsiusTemp);
 searchCity("Lviv");
